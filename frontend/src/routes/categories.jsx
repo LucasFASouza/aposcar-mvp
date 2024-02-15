17fafa48c3d7216a71f21bfd19b70e00595e1fb6
Lucas Souza <outrolucas2002@gmail.com>
2024-02-15 14:42:17 -0300

    import tailwind

--- frontend/src/routes/categories.jsx
+++ frontend/src/routes/categories.jsx
@@ -1,31 +1,22 @@
-import { useEffect } from "react";
-import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Categories() {
-  const { categories } = useLoaderData();
-
-  useEffect(() => {
-    console.log(categories);
-  }, [categories]);
-
   return (
     <>
       <div id="sidebar">
         <h1>Aposcar</h1>
         <nav>
-          {categories.length ? (
-            <ul>
-              {categories.map((category) => (
-                <li key={category.id}>
-                  <Link to={`categories/${category.id}`}>{category.name}</Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>Searching...</i>
-            </p>
-          )}
+          <ul>
+            <li>
+              <Link to="categories/1">Category 1</Link>
+            </li>
+            <li>
+              <Link to="categories/2">Category 2</Link>
+            </li>
+            <li>
+              <Link to="categories/3">Category 3</Link>
+            </li>
+          </ul>
         </nav>
       </div>
       <div id="detail">
