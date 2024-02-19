30c3f48abb8d3ddfb3958557c5043d7adb1a8fc9
Lucas Souza <outrolucas2002@gmail.com>
2024-02-19 12:20:11 -0300

    idek

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
